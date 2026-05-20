9f2663bf1da409b541f0fccc7711587fb06c8b0a
MiguelRosales1264 <miguelrosales1264@gmail.com>
2026-05-20 16:05:36 -0600

    getCryptoData function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 const API_KEY = 'CG-yY8T2xc9QH1fkNFNpq4gbYw4';
-const API_URL = 'https://api.coingecko.com/api/v3/';
 
-console.log('Crypto Dashboard');
+async function getCryptoData() {
+    const url = `https://api.coingecko.com/api/v3/coins/list?x_cg_demo_api_key=${API_KEY}`;
+
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		console.error('Error fetching crypto data:', error);
+	}
+}
+
+getCryptoData();
