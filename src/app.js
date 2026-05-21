727c11e838200efb74ec2b79eea1358bde51f6fd
MiguelRosales1264 <miguelrosales1264@gmail.com>
2026-05-21 13:23:00 -0600

    displayCryptoData & HTML/svgs

--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
 const API_KEY = 'CG-yY8T2xc9QH1fkNFNpq4gbYw4';
+const API_URL = 'https://api.coingecko.com/api/v3/';
+
+// Variables
+const cryptoDataDiv = document.getElementById('cryptoDataDiv');
 
 async function getCryptoData() {
-    const url = `https://api.coingecko.com/api/v3/coins/list?x_cg_demo_api_key=${API_KEY}`;
+	const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&names=Bitcoin&symbols=btc&category=layer-1&price_change_percentage=1h&x_cg_demo_api_key=${API_KEY}`;
 
 	try {
 		const response = await fetch(url);
 		const data = await response.json();
-		console.log(data);
+		return data;
 	} catch (error) {
 		console.error('Error fetching crypto data:', error);
+		return [];
 	}
 }
 
-getCryptoData();
+async function displayCryptoData() {
+	try {
+		const response = await getCryptoData();
+		response.forEach((crypto) => {
+			const { id, symbol, name, image } = crypto;
+			const cryptoInfoDiv = document.createElement('div');
+
+			cryptoInfoDiv.innerHTML = `
+				<img src='${image}' alt='${id}'>
+				<h2>${name}, (${symbol})</h2>
+			`
+
+			cryptoDataDiv.appendChild(cryptoInfoDiv);
+		})
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// displayCryptoData();
