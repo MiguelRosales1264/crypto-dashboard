9f8aabdf5064f9ab23cbc65ae345bffb93fc6d42
MiguelRosales1264 <miguelrosales1264@gmail.com>
2026-05-22 11:57:04 -0600

    remove unused vars

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const API_KEY = 'CG-yY8T2xc9QH1fkNFNpq4gbYw4';
 const API_URL = 'https://api.coingecko.com/api/v3/';
 
 // Variables
-const cryptoDataTable = document.getElementById('cryptoDataTable');
 const cryptoDataHeader = document.getElementById('cryptoDataHeader');
 const cryptoDataBody = document.getElementById('cryptoDataBody');
 
